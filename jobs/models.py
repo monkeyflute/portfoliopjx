29be3ef16c11b31aae9f17f2735312442476f456
Loren <loren.hopkins@gmail.com>
2018-04-25 10:56:57 +1000

    Jobs app up and running

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/') # create image object
+    summary  = models.CharField(max_length = 200) # caption the above image
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,10 +74,16 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# changed DB to postgress
+# replace this name: 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'portfoliodb',
+        'USER':'postgres',
+        'PASSWORD':'cYb3rUc^T',
+        'HOST':'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -118,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# remember we added these
+# got media root value from DATABASES IN THIS FILE
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
