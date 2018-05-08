105b1a3ac315d8b7900e9bc49da5fca282698c37
Loren <loren.hopkins@gmail.com>
2018-05-08 16:07:40 +1000

    first version of portfolio fully operational

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,17 @@ class Blog(models.Model):
     #title
     title = models.CharField(max_length=100)
     #date
-    timestamp= models.DateTimeField()
+    timestamp = models.DateTimeField()
     #body
     body = models.TextField()
     #image
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.timestamp.strftime('%b %e %Y')
